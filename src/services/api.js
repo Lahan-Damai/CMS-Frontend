ecd96767204cd76b7f499f3b8b1a877951e787fc
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2024-09-14 19:41:12 +0700

    lupa nambahin port di api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://34.126.138.137/";
+const API_BASE_URL = "http://34.126.138.137:3001/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
